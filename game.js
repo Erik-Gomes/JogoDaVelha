f7ff2588d4caa4467306bcd994c071b530523281
Erik-Gomes <you@example.com>
2021-09-29 22:52:32 -0300

    ajustes

--- game.js
+++ game.js
@@ -33,12 +33,12 @@ function inicializarEspacos(){
 
                 if (playTime == player1) {
 
-                    this.innerHTML = "<img src='imagens/X.png' border='0' height='70'>";
+                    this.innerHTML = "<img src='imagens/X.png' border='0' height='100'>";
                     this.setAttribute("jogada", player1);
                     playTime = player2;
 
                 }else{
-                    this.innerHTML = "<img src='imagens/O.png' border='0' height='70'>";
+                    this.innerHTML = "<img src='imagens/O.png' border='0' height='100'>";
                     this.setAttribute("jogada", player2);
                     playTime = player1;
 
@@ -51,43 +51,60 @@ function inicializarEspacos(){
         
 }
 
+
 async function verificarVencedor(){
 
-var a1 = document.getElementById("a1").getAttribute("jogada");
-var a2 = document.getElementById("a2").getAttribute("jogada");
-var a3 = document.getElementById("a3").getAttribute("jogada");
+    var a1 = document.getElementById("a1").getAttribute("jogada");
+    var a2 = document.getElementById("a2").getAttribute("jogada");
+    var a3 = document.getElementById("a3").getAttribute("jogada");
 
-var b1 = document.getElementById("b1").getAttribute("jogada");
-var b2 = document.getElementById("b2").getAttribute("jogada");
-var b3 = document.getElementById("b3").getAttribute("jogada");
+    var b1 = document.getElementById("b1").getAttribute("jogada");
+    var b2 = document.getElementById("b2").getAttribute("jogada");
+    var b3 = document.getElementById("b3").getAttribute("jogada");
 
-var c1 = document.getElementById("c1").getAttribute("jogada");
-var c2 = document.getElementById("c2").getAttribute("jogada");
-var c3 = document.getElementById("c3").getAttribute("jogada");
+    var c1 = document.getElementById("c1").getAttribute("jogada");
+    var c2 = document.getElementById("c2").getAttribute("jogada");
+    var c3 = document.getElementById("c3").getAttribute("jogada");
 
-var vencedor = "";
+    var vencedor = "";
 
-if((a1==b1 && a1==c1 && a1 !="" ) || ( a1==a2 && a1==a3 && a1 !="") || (a1==b2 && a1==c3 && a1 !="" ))
-{
-    vencedor = a1;
+    if((a1==b1 && a1==c1 && a1 !="" ) || ( a1==a2 && a1==a3 && a1 !="") || (a1==b2 && a1==c3 && a1 !="" ))
+        {
+            vencedor = a1;
 
-}else if((b2==b1 && b2==b3 && b2 !="") || (b2==a2 && b2==c2 && b2 !="" ) || (b2==a3 && b2==c1 && b2 !=""))
-{
-    vencedor = b2;
+        }else if((b2==b1 && b2==b3 && b2 !="") || (b2==a2 && b2==c2 && b2 !="" ) || (b2==a3 && b2==c1 && b2 !=""))
+        {
+            vencedor = b2;
 
-}else if(((c3==c2 && c3==c1) || (c3==a3 && c3==b3)) && c3!="")
-{
-    vencedor= c3;
+        }else if(((c3==c2 && c3==c1) || (c3==a3 && c3==b3)) && c3!="")
+        {
+            vencedor= c3;
 
-} if (vencedor!=""){
-    gameOver = true;
+        }else if(a1&&a2&&a3&&b1&&b2&&b3&&c1&&c2&&c3 !=""){
+            await sleep(50);
+            alert("empate")
+        }
+            
 
-    await sleep(50);
-    alert("O ganhador foi o: '" + vencedor + "'" )
-}
+
+
+
+     if (vencedor!=""){
+        gameOver = true;
+
+        await sleep(50);
+        console.log("o vencedor foi o:" + vencedor)
+    }
 }
 
 function sleep(ms) {
 
     return new Promise(resolve => setTimeout(resolve, ms));
 }
+
+
+function restart() {
+
+
+}
+

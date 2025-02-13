06cb7ba3e5cd7ce9423095e0426a12826cb44831
Erik Gomes <gomeserik35@gmail.com>
2025-02-13 16:19:53 -0300

    alteração css

--- game.js
+++ game.js
@@ -1,7 +1,10 @@
-const player1 = "X";
-const player2 = "O";
+const player1 = prompt("Nome do jogador 1");
+const player2 = prompt("Nome do jogador 2");
 var playTime = player1;
 var gameOver = false;
+var pontos1 = 0
+var pontos2 = 0
+ 
 
 atualizaMostrador();
 inicializarEspacos();
@@ -9,7 +12,8 @@ inicializarEspacos();
 function atualizaMostrador(){
 
    if(gameOver) {return;}
-
+   
+   
    if(playTime == player1) {
 
        var player = document.querySelectorAll("div#mostrador img")[0];
@@ -51,6 +55,8 @@ function inicializarEspacos(){
         
 }
 
+placar();
+
 
 async function verificarVencedor(){
 
@@ -89,12 +95,26 @@ async function verificarVencedor(){
 
 
 
-     if (vencedor!=""){
+    if (vencedor!=""){
         gameOver = true;
 
         await sleep(50);
-        console.log("o vencedor foi o:" + vencedor)
+        console.log("Parabéns " + vencedor + " você venceu!")
+      
+
+    } 
+
+    if (vencedor == player1){
+        pontos1 = pontos1 + 1 ;
+        
     }
+    
+    if (vencedor == player2){
+        pontos2 = pontos2 + 1;
+        
+    }
+
+    
 }
 
 function sleep(ms) {
@@ -104,7 +124,38 @@ function sleep(ms) {
 
 
 function restart() {
+    
+    document.getElementById("a1").innerHTML = "";
+    document.getElementById("a2").innerHTML = "";
+    document.getElementById("a3").innerHTML = "";
+    document.getElementById("b1").innerHTML = "";
+    document.getElementById("b2").innerHTML = "";
+    document.getElementById("b3").innerHTML = "";
+    document.getElementById("c1").innerHTML = "";
+    document.getElementById("c2").innerHTML = "";
+    document.getElementById("c3").innerHTML = "";
 
+    document.getElementById("a1").setAttribute("jogada", "");
+    document.getElementById("a2").setAttribute("jogada", "");
+    document.getElementById("a3").setAttribute("jogada", "");
+    document.getElementById("b1").setAttribute("jogada", "");
+    document.getElementById("b2").setAttribute("jogada", "");
+    document.getElementById("b3").setAttribute("jogada", "");
+    document.getElementById("c1").setAttribute("jogada", "");
+    document.getElementById("c2").setAttribute("jogada", "");
+    document.getElementById("c3").setAttribute("jogada", ""); 
+
+    placar();   
+    
+    gameOver = false; 
+        
+}   
+
+function placar() {
+
+    /*document.getElementById("placar").innerHTML = "placar:" ; */
+    document.getElementById("pontos1").innerHTML = player1 + ": " + pontos1 ;
+    document.getElementById("pontos2").innerHTML = player2 + ": " + pontos2 ;
 
 }
 
